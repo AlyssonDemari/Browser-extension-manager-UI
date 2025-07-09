c8d6fb9d9d1d4b0f12fe319754c929015bdfc9a7
AlyssonDemari <alyssondemari@gmail.com>
2025-07-08 22:49:31 -0300

    adicionando as funcionalidades em js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if (prefersDarkMode) {
+    console.log("O usuário está no modo escuro.");
+    // Você pode adicionar uma classe ao body para aplicar estilos específicos
+    document.body.classList.add('dark-mode');
+} else {
+    console.log("O usuário está no modo claro.");
+    document.body.classList.add('light-mode');
+}
+
+
 async function carregarDados() {
 
     const response = await fetch('../data.json');
@@ -13,9 +25,11 @@ async function carregarDados() {
         // 3. Determine a classe do ícone com base no 'isActive'
         const toggleIconClass = item.isActive ? 'fa-toggle-on' : 'fa-toggle-off';
 
+        const cardStatus  = item.isActive ? 'Active' : 'Inactive';
 
+        // 4. Crie o HTML do novo card
         const cardHTML = `
-            <div class="card">
+            <div class="card" data-status="${cardStatus}">
                 <header class="card__header">
                     <img src="${item.logo}" alt="">
                     <div class="card__header__text">
@@ -29,12 +43,84 @@ async function carregarDados() {
                 </footer>
             </div>
         `;
-
         // 5. Insira o HTML do novo card dentro do contêiner
         container.innerHTML += cardHTML;
     })
-}
 
+    // 6. responsável por remover o card
+    const removeCard = document.querySelectorAll('.btn-remove');
+    removeCard.forEach(removeCard => {
+        removeCard.addEventListener('click', () => {
+            const card = removeCard.closest('.card');
+            card.remove();
+        });
+    })
+
+    // 1. Adicione um único 'escutador' de eventos ao contêiner principal
+    container.addEventListener('click', (event) => {
+
+        console.log(event.target);
+        
+        // 2. Verifique se o elemento clicado (event.target) é um ícone de toggle
+        if (event.target.classList.contains('fa-toggle-on') || event.target.classList.contains('fa-toggle-off')) {
+            
+            // 3. Se for, alterne as classes no elemento que foi clicado
+            event.target.classList.toggle('fa-toggle-on');
+            event.target.classList.toggle('fa-toggle-off');
+        }
+
+        const cardClicked = event.target.closest('.card');
+        if (cardClicked) {
+            const cardStatus = cardClicked.getAttribute('data-status');
+            console.log(cardStatus);
+            if (cardStatus === 'Active') {
+                cardClicked.setAttribute('data-status', 'Inactive');
+            } else {
+                cardClicked.setAttribute('data-status', 'Active');
+            }
+        }
+    });
+}
 carregarDados();
 
 
+// --- LÓGICA PARA DEIXAR O BOTÃO ATIVO ---
+const contButton = document.querySelector('.buttons');
+contButton.addEventListener('click', (event) => {
+    
+    const clicked = event.target
+    console.log(clicked);
+
+    // caso o lugar na qual eu tenha clicado não seja um botão, sai da função
+    if(clicked.tagName !== 'BUTTON'){
+        return;
+    }
+
+    document.querySelector('.active').classList.remove('active');
+
+    const button = event.target;    
+    button.classList.add('active');
+
+    const allCards = document.querySelectorAll('.card');
+
+    let contCards = 0;
+    const empty = document.querySelector('.empty');
+
+    allCards.forEach(card => {
+        const cardStatus = card.getAttribute('data-status');
+        if(cardStatus === button.textContent || button.textContent === 'All'){
+            card.style.display = 'flex';
+            contCards++;
+            console.log(contCards);
+        } else {
+            card.style.display = 'none';
+        }
+    })
+
+    if(contCards === 0){
+        empty.classList.add('active');
+    }
+    else {
+        empty.classList.remove('active');
+    }
+})
